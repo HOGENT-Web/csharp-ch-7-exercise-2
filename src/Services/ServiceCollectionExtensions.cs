b413d010b284ed0dfcc3821f0f72d74c60c37ed0
Benjamin Vertonghen <benjamin.vertonghen@append.be>
2022-11-09 03:13:06 +0100

    Add Exercise

---
+++ src/Services/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using BogusStore.Services.Products;
+using BogusStore.Shared.Products;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BogusStore.Services;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds all services to the DI container.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddBogusServices(this IServiceCollection services)
+    {
+        services.AddScoped<IProductService, ProductService>();
+        // Add more services here...
+
+        return services;
+    }
+}
+
